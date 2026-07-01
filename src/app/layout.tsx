5295697659162695449a8f38096590781c4df20c
Arminius1204 <akshat.raj@bsccmh.christuniversity.in>
2026-07-01 11:08:13 +0530

    Typewriter Text Animation on Homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "VeriLens AI",
   description: "AI-Powered Explainable News Verification Platform",
+  icons: {
+  icon: "/logo.png",
+  shortcut: "/logo.png",
+  apple: "/logo.png",
+},
   
 };
 
